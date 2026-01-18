2e6bab00f2e7fbd3f4004f8fea45fba527e206fb
Shiraz Yoel <shiraz.jaul@post.runi.ac.il>
2026-01-18 13:34:02 +0200

    commit

--- LanguageModel.java
+++ LanguageModel.java
@@ -108,7 +108,7 @@ public class LanguageModel {
     }
     String generatedText = initialText;
     String window = initialText.substring(initialText.length() - windowLength); // 
-     while (generatedText.length() < textLength) { // 
+     while (generatedText.length() <= textLength) { // 
         List probs = CharDataMap.get(window);
         if (probs == null) {
             break; // 
